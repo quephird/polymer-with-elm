8faa61ed9a70ba87ca7780664f35cf249a6c5038
danielle kefford <danielle_kefford@hotmail.com>
2015-08-12 15:07:03 -0400

    _Finally_ refactored Polymer module to something a little nicer... I think.

--- Polymer.elm
+++ Polymer.elm
@@ -1,21 +1,32 @@
-module Polymer (GoogleChart(..), PieChartOptions, pieChartDefaults, render, toOffsetsString) where
+module Polymer (GoogleChart(..), PieChartOptions, pieChartDefaults, render) where
 
 import Html exposing (Html, div, node, text)
 import Html.Attributes exposing (attribute, checked, class)
-import List exposing (map)
+import List exposing (map, map2)
 import String exposing (join, toLower)
 
-type alias Title = String
 type alias Labels = (String, String)
 type alias Data = List (String, Int)
+
+type alias Title = String
+type alias Is3D = Bool
+type alias PieHole = Float
+type alias PieStartAngle = Int
 type alias SliceOffsets = List (Int, Float)
 
+type PieChartOption
+  = Title' Title
+  | Is3D' Is3D
+  | PieHole' PieHole
+  | PieStartAngle' PieStartAngle
+  | SliceOffsets' SliceOffsets
+
 type alias PieChartOptions =
-  { title : String
-  , is3D : Bool
-  , pieHole : Float
-  , pieStartAngle : Int
-  , sliceOffsets : List (Int, Float)
+  { title : Title
+  , is3D : Is3D
+  , pieHole : PieHole
+  , pieStartAngle : PieStartAngle
+  , sliceOffsets : SliceOffsets
   }
 
 type GoogleChart = PieChart Data Labels PieChartOptions
@@ -23,18 +34,31 @@ type GoogleChart = PieChart Data Labels PieChartOptions
 pieChartDefaults : PieChartOptions
 pieChartDefaults = PieChartOptions "" False 0.0 0 []
 
-toOffsetsString : SliceOffsets -> String
-toOffsetsString offsets =
-  join ", " <| map (\(i,o) -> "\"" ++ toString i ++ "\": {\"offset\": " ++ toString o ++ "}") offsets
+toJson : PieChartOption -> String
+toJson option =
+  case option of
+    Title' t ->
+      "\"title\": \"" ++ t ++ "\""
+    Is3D' i ->
+      "\"is3D\": " ++ (toLower <| toString i)
+    PieHole' h ->
+      "\"pieHole\": " ++ toString h
+    PieStartAngle' a ->
+      "\"pieStartAngle\": " ++ toString a
+    SliceOffsets' os ->
+      "\"slices\": { " ++
+      (join ", " <| map (\(i,o) -> "\"" ++ toString i ++ "\": {\"offset\": " ++ toString o ++ "}") os) ++
+      "}"
 
 toOptionsString : PieChartOptions -> String
 toOptionsString {title, is3D, pieHole, pieStartAngle, sliceOffsets} =
-                   "{\"title\": \"" ++ title ++ "\", "
-                   ++ "\"is3D\": " ++ (toLower <| toString is3D) ++ ", "
-                   ++ "\"pieHole\": " ++ toString pieHole ++ ", "
-                   ++ "\"pieStartAngle\": " ++ toString pieStartAngle ++ ", "
-                   ++ "\"slices\": { " ++ toOffsetsString sliceOffsets ++ "}"
-                   ++ "}"
+  "{" ++
+  (join ", " <| map toJson [Title' title,
+                            Is3D' is3D,
+                            PieHole' pieHole,
+                            PieStartAngle' pieStartAngle,
+                            SliceOffsets' sliceOffsets]) ++
+  "}"
 
 toDataString : (String, String) -> List (String, Int) -> String
 toDataString (xLabel, yLabel) tuples =
@@ -49,7 +73,9 @@ render (PieChart data labels options) =
   let
     optionsString = toOptionsString options
     dataString = toDataString labels data
-    attributes = map (\[n,v] -> attribute n v) [["type", "pie"], ["options", optionsString], ["data", dataString]]
+    attributes = map (\[n,v] -> attribute n v) [["type", "pie"],
+                                                ["options", optionsString],
+                                                ["data", dataString]]
     children   = []
   in
     node "google-chart" attributes children
